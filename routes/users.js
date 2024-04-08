871b5354b91fa122e6e4cc5f1c2e102ce844b1b1
Ali Hassan <www.ali.hassan.2005@gmail.com>
2024-04-08 02:46:35 +0200

    feature/user

--- routes/users.js
+++ routes/users.js
@@ -5,18 +5,27 @@ var isAuthenticated = require("../middlewares/authentication");
 var authorization = require("../middlewares//authorization");
 
 //create new user
-router.post("/",isAuthenticated.user,authorization('admin') ,userController.store);
+router.post(
+  "/",
+  isAuthenticated.user,
+  authorization("admin"),
+  userController.store
+);
 //login user
 router.post("/login", userController.login);
 //forgot password
 router.post("/forgotPassword", userController.forgotPassword);
 //reset password
 router.put("/resetPassword", userController.resetPassword);
-//update user by admin
+//update userRole by admin
+
 //update user
+router.put("/update", isAuthenticated, userController.update);
 //get user by admin
 //get user
 //get all users
+//delete user by admin
+//delete user
 //active
-//non active 
+//non active
 module.exports = router;
