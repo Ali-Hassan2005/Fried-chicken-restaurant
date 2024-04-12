64efc2ab810d340e8fcd0f75e14a85b79876e9e5
Omar Ehab <mooroehab18@gmail.com>
2024-04-12 04:41:07 +0200

    logger

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
+var logger = require("moragn");
 
 var db = require("./configs/db");
 
@@ -17,7 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-
+app.use(logger("dev"));
 //routes
 app.use("/api/client", clientRouter);
 app.use("/api/user", userRouter);
