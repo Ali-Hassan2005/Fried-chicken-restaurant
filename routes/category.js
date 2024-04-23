01b47e4374a8486f70fe59c67e6f0357648a026f
Ali Hassan <www.ali.hassan.2005@gmail.com>
2024-04-23 18:16:46 +0200

    feature/addPreview

--- routes/category.js
+++ routes/category.js
@@ -4,7 +4,10 @@ var categoryController = require("../controllers/category.controller");
 var isAuthenticated = require("../middlewares/authentication");
 var authorization = require("../middlewares/authorization");
 
-router.post("/addCategory", categoryController.store);
-router.get("/getCategory", categoryController.update);
+router.get("/", categoryController.getAllCategory);
+router.use(isAuthenticated("user", authorization("admin")));
+router.post("/", categoryController.store);
+router.put("/:id", categoryController.update);
+router.delete("/:id", categoryController.delete);
 
 module.exports = router;
--- routes/foods.js
+++ routes/foods.js
@@ -1,5 +1,12 @@
 const { Router } = require("express");
-const { store, getAllFood } = require("../controllers/food.controller");
+const {
+  store,
+  getAllFood,
+  getFood,
+  update,
+  deletefood,
+  addPreview,
+} = require("../controllers/food.controller");
 var isAuthenticated = require("../middlewares/authentication");
 var authorization = require("../middlewares//authorization");
 
@@ -7,6 +14,20 @@ const routes = Router();
 
 // store
 routes.post("/", isAuthenticated("user"), authorization("admin"), store);
+//get all food
 routes.get("/", getAllFood);
+//get food
+routes.get("/:id", getFood);
+// update food
+routes.put("/:id", isAuthenticated("user"), authorization("admin"), update);
+// delete food
+routes.delete(
+  "/:id",
+  isAuthenticated("user"),
+  authorization("admin"),
+  deletefood
+);
+// add rate
+routes.post("/preview/:id",isAuthenticated("client"), addPreview);
 
 module.exports = routes;
